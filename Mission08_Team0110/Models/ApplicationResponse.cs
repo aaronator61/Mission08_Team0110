c2bb5c9a2f824f95ae661df41dd4ef6b5caff896
Nathan Kunz <nathanck@byu.edu>
2023-02-23 12:50:11 -0700

    model/database

---
+++ Mission08_Team0110/Models/ApplicationResponse.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission08_Team0110.Models
+{
+    public class ApplicationResponse
+    {
+        [Key]
+        [Required]
+        public int taskId { get; set; }
+        [Required]
+        public string task {get; set; }
+        public string DueDate { get; set; }
+        [Required]
+        public byte Quadrant { get; set; }
+        public bool Completed  { get; set; }
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+
+
+    }
+}
---
+++ Mission08_Team0110/Models/Category.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission08_Team0110.Models
+{
+    public class Category
+    {
+        [Key]
+        [Required]
+        public int Id { get; set; }
+        public string CatName { get; set; }
+    }
+}
